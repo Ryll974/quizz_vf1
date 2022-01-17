1a14c3de8454fc432ca184122f0f18c12bea9632
Ryll974 <jullien.patrick@gmail.com>
2022-01-17 13:46:33 +0400

    3  - Les Listes (en cours)

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,10 @@ class Quizz extends StatefulWidget {
 }
 
 class _Quizz extends State<Quizz> {
+  final Icon iconRight = Icon(Icons.check,color:Colors.green,size: 25.0,);
+  final Icon iconWrong = Icon(Icons.close,color:Colors.red,size: 25.0,);
+  List<Icon> listedesicones = [];
+  listedesicones = [iconRight];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -83,33 +87,7 @@ class _Quizz extends State<Quizz> {
                   )),
             )),
         Row(
-          children:[
-            Icon(
-              Icons.check,
-              color: Colors.green,
-              size: 25.0,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-              size: 25.0,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-              size: 25.0,
-            ),
-            Icon(
-              Icons.check,
-              color: Colors.green,
-              size: 25.0,
-            ),
-            Icon(
-              Icons.check,
-              color: Colors.green,
-              size: 25.0,
-            ),
-          ],
+          children: listedesicones,
         ),
       ],
     );
